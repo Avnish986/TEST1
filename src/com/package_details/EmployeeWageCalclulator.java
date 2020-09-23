0c5cf28c8b4313ae721bc898ab00a718e41cd4e3
avnish986 <avnishgupta986@gmail.com>
2020-09-23 11:18:16 +0530

    uc5 add

--- src/com/package_details/EmployeeWageCalclulator.java
+++ src/com/package_details/EmployeeWageCalclulator.java
@@ -2,28 +2,41 @@ package com.package_details;
 
 public class EmployeeWageCalclulator {
 
+	public static final int Is_Part_Time=1;
+	public static final int Is_Full_Time=2;
+	public static final int Emp_Rate_Per_Hour=20;
+	public static final int Num_Of_Working_Days=2;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int Is_Full_Time=1;
-		int Is_Part_Time=1;
-		int Emp_Rate_Per_hour=20;
-		int empHrs=0;
-		int empWage=0;
-		int empCheck=(int) Math.floor(Math.random()*10)%3;
-		switch(empCheck) {
-		case Is_Part_Time:
-		empHrs=4;
-		break;
+	// TODO Auto-generated method stub
 
-		case Is_Full_Time:
-		empHrs=8;
-		break;
+	int empHrs=0;
+	int empWage=0;
+	int totalEmpWage=0;
 
-		default:
-		empHrs=0;
-		}
-		empWage=empHrs*Emp_Rate_Per_Hour;
-		System.out.println("Emp Wage : "+empWage);
+	for(int day=0;day<Num_Of_Working_Days;day++)
+	{
+	int empCheck=(int)Math.floor(Math.random()*10)%3;
+	switch(empCheck) {
+	case Is_Part_Time:
+	empHrs=4;
+	break;
+
+	case Is_Full_Time:
+	empHrs=8;
+	break;
+
+	default:
+	empHrs=0;
+	}
+	empWage=empHrs*Emp_Rate_Per_Hour;
+	totalEmpWage+=empWage;
+	System.out.println("Emp Wage : "+empWage);
+
+
+	}
+	System.out.println("Total Emp Wage : "+totalEmpWage);
+
+
 	}
 
 }
